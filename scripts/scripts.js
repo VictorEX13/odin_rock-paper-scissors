f44ee2d73624c8698b419716897402b35a21b27e
Victor <victorshiira4869@gmail.com>
2023-01-10 10:28:12 -0300

    Create function to play a round of Rock-Paper-Scissors

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,3 +11,27 @@ function getComputerChoice() {
 function capitalize(str) {
   return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+function playRound(playerSelection, computerSelection) {
+  const capitalizedPlayerSelection = capitalize(playerSelection);
+
+  if (computerSelection === "Rock") {
+    return capitalizedPlayerSelection === "Rock"
+      ? "A tie!"
+      : capitalizedPlayerSelection === "Paper"
+      ? "You win! Paper beats Rock"
+      : "You lose! Rock beats Scissors";
+  } else if (computerSelection === "Paper") {
+    return capitalizedPlayerSelection === "Rock"
+      ? "You lose! Paper beats Rock"
+      : capitalizedPlayerSelection === "Paper"
+      ? "A tie!"
+      : "You win! Scissors beats Paper";
+  } else {
+    return capitalizedPlayerSelection === "Rock"
+      ? "You win! Rock beats Scissors"
+      : capitalizedPlayerSelection === "Paper"
+      ? "You lose! Scissors beats Paper"
+      : "A tie!";
+  }
+}
